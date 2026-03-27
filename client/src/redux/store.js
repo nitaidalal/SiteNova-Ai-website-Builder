2d51fb111585d10ee8678ad26678d985a5b7e641
nitaidalal <dalalnitai7@gmail.com>
2026-03-28 00:13:33 +0530

    refactor: remove authSlice and update store configuration to use userSlice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import themeReducer from './themeSlice'
-import authReducer from './authSlice'
+import userReducer from './userSlice'
 
 const store = configureStore({
-    reducer:{
-        theme: themeReducer,
-        auth: authReducer
-    }
-})
+  reducer: {
+    theme: themeReducer,
+    user: userReducer,
+  },
+});
 
 export default store
